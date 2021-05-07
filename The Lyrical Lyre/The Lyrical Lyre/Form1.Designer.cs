be10502900bcbcffbb559a54cd4bf722ef407361
824351 <81653262+824351@users.noreply.github.com>
2021-05-07 14:19:50 -0700

    added start button

--- The Lyrical Lyre/The Lyrical Lyre/Form1.Designer.cs
+++ The Lyrical Lyre/The Lyrical Lyre/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace The_Lyrical_Lyre
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,36 @@ namespace The_Lyrical_Lyre
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStart.Font = new System.Drawing.Font("MV Boli", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(75, 71);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(156, 74);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(317, 224);
+            this.Controls.Add(this.btnStart);
+            this.Name = "FormMain";
+            this.Text = "Reykjavik Salvador & Keziah Vibieda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- The Lyrical Lyre/The Lyrical Lyre/Form1.cs
+++ The Lyrical Lyre/The Lyrical Lyre/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace The_Lyrical_Lyre
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+
+
+
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnStart.Left = (this.ClientRectangle.Width / 2) - (btnStart.Width / 2);
+            btnStart.Top = (this.ClientRectangle.Height / 2) - (btnStart.Height / 2);
         }
     }
 }
