cc8e5505d97aaa66053a502c245f4409558b97d4
WayCoolCoding <masterjohnson@waycoolcoding.com>
2021-05-18 12:48:04 -0700

    Retrieve Work by Branching off older Commit

--- The Lyrical Lyre/The Lyrical Lyre/Program.cs
+++ The Lyrical Lyre/The Lyrical Lyre/Program.cs
@@ -16,8 +16,8 @@ namespace The_Lyrical_Lyre
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormMain());
-            Application.Run(new Music_Player.Form1());
+            Application.Run(new FormMain());
+            //Application.Run(new Music_Player.Form1());
         }
     }
 }
