650038c49bfd1cb20afd3f8a42d124906685eb72
824351 <81653262+824351@users.noreply.github.com>
2021-05-18 15:03:24 -0700

    Game Development

--- The Lyrical Lyre/The Lyrical Lyre/Song1Game.Designer.cs
+++ The Lyrical Lyre/The Lyrical Lyre/Song1Game.Designer.cs
@@ -29,6 +29,7 @@ namespace The_Lyrical_Lyre
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picABox = new System.Windows.Forms.PictureBox();
             this.picWBox = new System.Windows.Forms.PictureBox();
             this.picSBox = new System.Windows.Forms.PictureBox();
@@ -38,6 +39,8 @@ namespace The_Lyrical_Lyre
             this.picKBox = new System.Windows.Forms.PictureBox();
             this.picLBox = new System.Windows.Forms.PictureBox();
             this.picBorder = new System.Windows.Forms.PictureBox();
+            this.animationTimer = new System.Windows.Forms.Timer(this.components);
+            this.tempoTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picABox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picWBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picSBox)).BeginInit();
@@ -51,9 +54,8 @@ namespace The_Lyrical_Lyre
             // 
             // picABox
             // 
-            this.picABox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picABox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picABox.BackColor = System.Drawing.Color.Cyan;
             this.picABox.Location = new System.Drawing.Point(113, 12);
             this.picABox.Name = "picABox";
             this.picABox.Size = new System.Drawing.Size(78, 72);
@@ -62,21 +64,20 @@ namespace The_Lyrical_Lyre
             // 
             // picWBox
             // 
-            this.picWBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picWBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picWBox.BackColor = System.Drawing.Color.GreenYellow;
             this.picWBox.Location = new System.Drawing.Point(12, 12);
             this.picWBox.Name = "picWBox";
             this.picWBox.Size = new System.Drawing.Size(78, 72);
             this.picWBox.TabIndex = 1;
             this.picWBox.TabStop = false;
+            this.picWBox.Click += new System.EventHandler(this.picWBox_Click);
             // 
             // picSBox
             // 
-            this.picSBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.picSBox.Location = new System.Drawing.Point(226, 12);
+            this.picSBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picSBox.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.picSBox.Location = new System.Drawing.Point(217, 12);
             this.picSBox.Name = "picSBox";
             this.picSBox.Size = new System.Drawing.Size(78, 72);
             this.picSBox.TabIndex = 2;
@@ -84,10 +85,9 @@ namespace The_Lyrical_Lyre
             // 
             // picDBox
             // 
-            this.picDBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.picDBox.Location = new System.Drawing.Point(339, 12);
+            this.picDBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picDBox.BackColor = System.Drawing.Color.MidnightBlue;
+            this.picDBox.Location = new System.Drawing.Point(321, 12);
             this.picDBox.Name = "picDBox";
             this.picDBox.Size = new System.Drawing.Size(78, 72);
             this.picDBox.TabIndex = 3;
@@ -95,10 +95,9 @@ namespace The_Lyrical_Lyre
             // 
             // picJBox
             // 
-            this.picJBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.picJBox.Location = new System.Drawing.Point(446, 12);
+            this.picJBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picJBox.BackColor = System.Drawing.Color.Orange;
+            this.picJBox.Location = new System.Drawing.Point(529, 12);
             this.picJBox.Name = "picJBox";
             this.picJBox.Size = new System.Drawing.Size(78, 72);
             this.picJBox.TabIndex = 4;
@@ -106,10 +105,9 @@ namespace The_Lyrical_Lyre
             // 
             // picIBox
             // 
-            this.picIBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.picIBox.Location = new System.Drawing.Point(545, 12);
+            this.picIBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picIBox.BackColor = System.Drawing.Color.Brown;
+            this.picIBox.Location = new System.Drawing.Point(425, 12);
             this.picIBox.Name = "picIBox";
             this.picIBox.Size = new System.Drawing.Size(78, 72);
             this.picIBox.TabIndex = 5;
@@ -117,10 +115,9 @@ namespace The_Lyrical_Lyre
             // 
             // picKBox
             // 
-            this.picKBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.picKBox.Location = new System.Drawing.Point(644, 12);
+            this.picKBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picKBox.BackColor = System.Drawing.Color.Chartreuse;
+            this.picKBox.Location = new System.Drawing.Point(640, 12);
             this.picKBox.Name = "picKBox";
             this.picKBox.Size = new System.Drawing.Size(78, 72);
             this.picKBox.TabIndex = 6;
@@ -128,9 +125,8 @@ namespace The_Lyrical_Lyre
             // 
             // picLBox
             // 
-            this.picLBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picLBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picLBox.BackColor = System.Drawing.Color.Navy;
             this.picLBox.Location = new System.Drawing.Point(746, 12);
             this.picLBox.Name = "picLBox";
             this.picLBox.Size = new System.Drawing.Size(78, 72);
@@ -139,13 +135,24 @@ namespace The_Lyrical_Lyre
             // 
             // picBorder
             // 
-            this.picBorder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBorder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.picBorder.BackColor = System.Drawing.Color.Black;
             this.picBorder.Location = new System.Drawing.Point(-1, 388);
             this.picBorder.Name = "picBorder";
             this.picBorder.Size = new System.Drawing.Size(844, 10);
             this.picBorder.TabIndex = 8;
             this.picBorder.TabStop = false;
+            // 
+            // animationTimer
+            // 
+            this.animationTimer.Interval = 5;
+            this.animationTimer.Tick += new System.EventHandler(this.animationTimer_Tick);
+            // 
+            // tempoTimer
+            // 
+            this.tempoTimer.Interval = 3000;
+            this.tempoTimer.Tick += new System.EventHandler(this.tempoTimer_Tick);
             // 
             // Song1Game
             // 
@@ -163,6 +170,8 @@ namespace The_Lyrical_Lyre
             this.Controls.Add(this.picABox);
             this.Name = "Song1Game";
             this.Text = "Song1Game";
+            this.Load += new System.EventHandler(this.Song1Game_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Song1Game_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.picABox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picWBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picSBox)).EndInit();
@@ -187,5 +196,7 @@ namespace The_Lyrical_Lyre
         private System.Windows.Forms.PictureBox picKBox;
         private System.Windows.Forms.PictureBox picLBox;
         private System.Windows.Forms.PictureBox picBorder;
+        private System.Windows.Forms.Timer animationTimer;
+        private System.Windows.Forms.Timer tempoTimer;
     }
 }
