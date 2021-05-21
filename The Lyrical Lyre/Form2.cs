7447ae86fce0f03c611a483f145c75726ece221a
824351 <81653262+824351@users.noreply.github.com>
2021-05-21 15:05:22 -0700

    IMPORT

---
+++ The Lyrical Lyre/Form2.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace The_Lyrical_Lyre
+{
+
+    // REMEMBER TO CHANGE THE NAME OF "Song1", "Song2" etc. TO THE ACTUAL SONG!!!
+
+    public partial class FormSelect : Form
+    {
+
+        // Create Global Variables
+        bool song1 = false, song2 = false, song3 = false, song4 = false, difficultyIn, selected = false;
+        int tick = 0, tick2 = 0;
+
+        // Global Variables to OPEN game
+        Song1Game open;
+
+        private void btnSong1_Click(object sender, EventArgs e)
+        {
+            song1 = true;
+            song2 = false;
+            song3 = false;
+            song4 = false;
+            animateDifficultyIn();
+            animateSongsOut();
+        }
+
+        private void btnSong2_Click(object sender, EventArgs e)
+        {
+            song1 = false;
+            song2 = true;
+            song3 = false;
+            song4 = false;
+            animateDifficultyIn();
+            animateSongsOut();
+        }
+
+        private void btnSong3_Click(object sender, EventArgs e)
+        {
+            song1 = false;
+            song2 = false;
+            song3 = true;
+            song4 = false;
+            animateDifficultyIn();
+            animateSongsOut();
+        }
+
+        private void btnSong4_Click(object sender, EventArgs e)
+        {
+            song1 = false;
+            song2 = false;
+            song3 = false;
+            song4 = true;
+            animateDifficultyIn();
+            animateSongsOut();
+        }
+
+        private void timerAnimateDifficulty_Tick(object sender, EventArgs e)
+        {
+            if (selected == false)
+            {
+                tick += 1;
+                lbDifficulty.Left = lbDifficulty.Left + 10;
+                btnEasy.Left += 10;
+                btnMedium.Left += 10;
+                btnHard.Left += 10;
+
+                if (tick == 20)
+                {
+                    timerAnimateDifficulty.Stop();
+                    tick = 0;
+                }
+            }
+
+        }
+
+        private void timerDetectBool_Tick(object sender, EventArgs e)
+        {
+            if (song1)
+            {
+                btnSong1.Click -= btnSong1_Click;
+            }
+            else if (song2)
+            {
+                btnSong2.Click -= btnSong2_Click;
+            }
+            else if (song3)
+            {
+                btnSong3.Click -= btnSong3_Click;
+            }
+            else if (song4)
+            {
+                btnSong4.Click -= btnSong4_Click;
+            }
+        }
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+            if (song1)
+            {
+                open = new Song1Game();
+                open.Show();
+            
+            }
+
+        }
+
+        SoundPlayer Music; // Plays selected song
+
+        // REAL TIMER NAME: TimerAnimateSong (animates songs)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (selected == false) // Animates Song CHoices when one is selected
+            {
+                tick2 += 1;
+                lbSong.Left += 6;
+
+                if (song1)
+                {
+                    btnSong2.Left += 10;
+                    btnSong3.Left += 10;
+                    btnSong4.Left += 10;
+                }
+                else if (song2)
+                {
+                    btnSong1.Left += 10;
+                    btnSong3.Left += 10;
+                    btnSong4.Left += 10;
+                }
+                else if (song3)
+                {
+                    btnSong2.Left += 10;
+                    btnSong1.Left += 10;
+                    btnSong4.Left += 10;
+                }
+                else if (song4)
+                {
+                    btnSong2.Left += 10;
+                    btnSong3.Left += 10;
+                    btnSong1.Left += 10;
+                }
+
+
+                if (tick2 == 20)
+                {
+                    timerAnimateSong.Stop();
+                    tick2 = 0;
+                    selected = true;
+                    btnSong1.Click += btnSong1_Click;
+                    btnSong2.Click += btnSong2_Click;
+                    btnSong3.Click += btnSong3_Click;
+                    btnSong4.Click += btnSong4_Click;
+                }
+            }
+            else if (selected)  // Animates the song choices once one has already been selected
+            {
+                tick2 += 1;
+
+                if (song1)
+                {
+                    btnSong1.Left -= 10;
+
+                    if (btnSong4.Left != 659)
+                    {
+                        btnSong4.Left += 10;
+                    }
+                    else if (btnSong2.Left != 659)
+                    {
+                        btnSong2.Left += 10;
+                    }
+                    else if (btnSong3.Left != 659)
+                    {
+                        btnSong3.Left += 10;
+                    }
+                }
+                else if (song2)
+                {
+                    btnSong2.Left -= 10;
+
+                    if (btnSong1.Left != 659)
+                    {
+                        btnSong1.Left += 10;
+                    }
+                    else if (btnSong4.Left != 659)
+                    {
+                        btnSong4.Left += 10;
+                    }
+                    else if (btnSong3.Left != 659)
+                    {
+                        btnSong3.Left += 10;
+                    }
+                }
+                else if (song3)
+                {
+                    btnSong3.Left -= 10;
+
+                    if (btnSong1.Left != 659)
+                    {
+                        btnSong1.Left += 10;
+                    }
+                    else if (btnSong2.Left != 659)
+                    {
+                        btnSong2.Left += 10;
+                    }
+                    else if (btnSong4.Left != 659)
+                    {
+                        btnSong4.Left += 10;
+                    }
+                }
+                else if (song4)
+                {
+                    btnSong4.Left -= 10;
+
+                    if (btnSong1.Left != 659)
+                    {
+                        btnSong1.Left += 10;
+                    }
+                    else if (btnSong2.Left != 659)
+                    {
+                        btnSong2.Left += 10;
+                    }
+                    else if (btnSong3.Left != 659)
+                    {
+                        btnSong3.Left += 10;
+                    }
+                }
+
+                // Stop and Reset Timer
+                if (tick2 == 20)
+                {
+                    timerAnimateSong.Stop();
+                    tick2 = 0;
+
+                    // Reenables the clicking of buttons
+                    btnSong1.Click += btnSong1_Click;
+                    btnSong2.Click += btnSong2_Click;
+                    btnSong3.Click += btnSong3_Click;
+                    btnSong4.Click += btnSong4_Click;
+                }
+
+            }
+
+        }
+
+        public FormSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSelect_Load(object sender, EventArgs e)
+        {
+            // Hide Difficulties
+            lbDifficulty.Visible = false;
+            btnEasy.Visible = false;
+            btnMedium.Visible = false;
+            btnHard.Visible = false;
+
+            // Start Detection Timer
+            timerDetectBool.Start();
+        }
+
+        // METHODS BELOW HERE
+
+        // Method animates the difficulty title and buttons.
+        private void animateDifficultyIn()
+        {
+            lbDifficulty.Visible = true;
+            btnEasy.Visible = true;
+            btnMedium.Visible = true;
+            btnHard.Visible = true;
+            difficultyIn = true;
+            timerAnimateDifficulty.Start();
+        }
+
+        // Method animates the song choice buttons
+        private void animateSongsOut()
+        {
+            btnSong1.Click -= btnSong1_Click;
+            btnSong2.Click -= btnSong2_Click;
+            btnSong3.Click -= btnSong3_Click;
+            btnSong4.Click -= btnSong4_Click;
+            timerAnimateSong.Start();
+        }
+    }
+}
---
+++ The Lyrical Lyre/Song1Game.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Lyrical_Lyre
+{
+
+        public partial class Song1Game : Form
+    {
+        
+        // Create Global Variables
+        Random generate = new Random();
+        List<PictureBox> notes = new List<PictureBox>();
+        List<Label> keyNotes = new List<Label>();
+        PictureBox selectedNote, randomNote;
+        int tempo = 5, max, score;
+        
+
+        private void Song1Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                lbStart.Visible = false;
+                tempoTimer.Start();
+            }
+            if(e.KeyCode == Keys.W)
+            {
+                if (picWBox.Top >= (picBorder.Top - picWBox.Top) - 5 && picWBox.Top <= (picBorder.Top - picWBox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picWBox.Top > (picBorder.Top - picWBox.Top) + 5 && picWBox.Top < picBorder.Bottom)
+                {
+                    great();                
+                }
+                else if (picWBox.Top > picBorder.Bottom || picWBox.Top < picBorder.Top - picWBox.Top - 5)
+                {
+                    failure();
+                }
+            }
+
+            if(e.KeyCode == Keys.A)
+            {
+                if (picABox.Top >= (picBorder.Top - picABox.Top) - 5 && picABox.Top <= (picBorder.Top - picABox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picABox.Top > (picBorder.Top - picABox.Top) + 5 && picABox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picABox.Top > picBorder.Bottom || picABox.Top < picBorder.Top - picABox.Top)
+                {
+                    failure();
+                }
+            }
+            
+            if (e.KeyCode == Keys.S)
+            {
+                if (picSBox.Top >= (picBorder.Top - picSBox.Top) - 5 && picSBox.Top <= (picBorder.Top - picSBox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picSBox.Top > (picBorder.Top - picSBox.Top) + 5 && picSBox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picSBox.Top > picBorder.Bottom || picSBox.Top < picBorder.Top - picSBox.Top)
+                {
+                    failure();
+                }
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                if (picDBox.Top >= (picBorder.Top - picDBox.Top) - 5 && picDBox.Top <= (picBorder.Top - picDBox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picDBox.Top > (picBorder.Top - picDBox.Top) + 5 && picDBox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picDBox.Top > picBorder.Bottom || picDBox.Top < picBorder.Top - picDBox.Top)
+                {
+                    failure();
+                }
+            }
+
+            if (e.KeyCode == Keys.I)
+            {
+                if (picIBox.Top >= (picBorder.Top - picIBox.Top) - 5 && picIBox.Top <= (picBorder.Top - picIBox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picIBox.Top > (picBorder.Top - picIBox.Top) + 5 && picIBox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picIBox.Top > picBorder.Bottom || picIBox.Top < picBorder.Top - picIBox.Top)
+                {
+                    failure();
+                }
+            }
+
+            if (e.KeyCode == Keys.J)
+            {
+                if (picJBox.Top >= (picBorder.Top - picJBox.Height) - 5 && picJBox.Top <= (picBorder.Top - picJBox.Height) + 5)
+                {
+                    perfect();
+                }
+                else if (picJBox.Top > (picBorder.Top - picJBox.Top) + 5 && picJBox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picJBox.Top > picBorder.Bottom || picJBox.Top < picBorder.Top - picJBox.Top)
+                {
+                    failure();
+                }
+            }
+
+            if (e.KeyCode == Keys.K)
+            {
+                if (picKBox.Top >= (picBorder.Top - picKBox.Top) - 5 && picKBox.Top <= (picBorder.Top - picKBox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picKBox.Top > (picBorder.Top - picKBox.Top) + 5 && picKBox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picKBox.Top > picBorder.Bottom || picKBox.Top < picBorder.Top - picKBox.Top)
+                {
+                    failure();
+                }
+            }
+
+            if (e.KeyCode == Keys.L)
+            {
+                if (picLBox.Top >= (picBorder.Top - picLBox.Top) - 5 && picLBox.Top <= (picBorder.Top - picLBox.Top) + 5)
+                {
+                    perfect();
+                }
+                else if (picLBox.Top > (picBorder.Top - picLBox.Top) + 5 && picLBox.Top < picBorder.Bottom)
+                {
+                    great();
+                }
+                else if (picLBox.Top > picBorder.Bottom || picLBox.Top < picBorder.Top - picLBox.Top)
+                {
+                    failure();
+                }
+            }
+        }
+
+        private void tempoTimer_Tick(object sender, EventArgs e)
+        {
+            // Create variables
+            randomNote = notes[generate.Next(max)];
+            callNote(randomNote);
+            picBorder.BackColor = Color.Black;
+            for (var i = 0; i < keyNotes.Count; i++)
+            {
+                keyNotes[i].BackColor = Color.Black;
+            }
+        }
+
+        private void picWBox_Click(object sender, EventArgs e)
+        {
+            callNote(picWBox);
+        }
+
+        public Song1Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Song1Game_Load(object sender, EventArgs e)
+        {
+            // Populate pictureBox list 
+            notes.Add(picWBox);
+            notes.Add(picABox);
+            notes.Add(picSBox);
+            notes.Add(picDBox);
+            notes.Add(picIBox);
+            notes.Add(picJBox);
+            notes.Add(picKBox);
+            notes.Add(picLBox);
+
+            // Populate Label List
+            keyNotes.Add(lbW);
+            keyNotes.Add(lbA);
+            keyNotes.Add(lbS);
+            keyNotes.Add(lbD);
+            keyNotes.Add(lbI);
+            keyNotes.Add(lbJ);
+            keyNotes.Add(lbK);
+            keyNotes.Add(lbL);
+
+
+
+            // Set Defaults
+            score = 0;
+            setDefaults();
+
+            this.KeyPreview = true;
+
+        }
+
+        private void animationTimer_Tick(object sender, EventArgs e)
+        {
+            //tick++;
+
+            selectedNote.Top += tempo;
+
+            if (selectedNote.Top >= picBorder.Top)
+            {
+                animationTimer.Stop();
+                resetNote(selectedNote);
+                setDefaults();
+                failure();
+            }
+            
+
+        }
+
+        // METHODS BELOW HERE
+        // this method calls a note down
+        private void callNote(PictureBox Note)
+        {
+            selectedNote = Note;
+            selectedNote.Visible = true;
+            animationTimer.Start();
+        }
+
+        // Set Default
+        private void setDefaults()
+        { 
+            max = notes.Count;
+            for (var index = 0; index < max; index++)
+            {
+               notes[index].Top = 0;
+               notes[index].Visible = false;
+               
+            }
+            for (var i = 0; i < keyNotes.Count; i++)
+            {
+                keyNotes[i].BackColor = Color.Black;
+            }
+            lbScore.Text = "Score: " + score;
+
+
+        }
+
+        // Reset Picturebox
+        private void resetNote(PictureBox note)
+        {
+            note.Visible = false;
+            note.Top = 0;
+        }
+
+        // Perfect Score
+        private void perfect()
+        {
+            resetNote(selectedNote);
+            animationTimer.Stop();
+            picBorder.BackColor = Color.Blue;
+            for (var i = 0; i < keyNotes.Count; i++)
+            {
+                keyNotes[i].BackColor = Color.Blue;
+            }
+            score += 100;
+            lbScore.Text = "Score: " + score;
+        }
+
+        // Great!
+        private void great()
+        {
+            resetNote(selectedNote);
+            animationTimer.Stop();
+            picBorder.BackColor = Color.Yellow;
+            for (var i = 0; i < keyNotes.Count; i++)
+            {
+                keyNotes[i].BackColor = Color.Yellow;
+            }
+            score += 25;
+            lbScore.Text = "Score: " + score;
+        }
+
+        // Failure
+        private void failure()
+        {
+            resetNote(selectedNote);
+            animationTimer.Stop();
+            picBorder.BackColor = Color.Red;
+            for (var i = 0; i < keyNotes.Count; i++)
+            {
+                keyNotes[i].BackColor = Color.Red;
+            }
+            score -= 10;
+            lbScore.Text = "Score: " + score;
+        }
+    }
+}
