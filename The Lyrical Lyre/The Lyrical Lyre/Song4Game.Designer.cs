40c67ac806ae622158e3b72919d0ee6295efe30d
kexoxo460505 <74347099+kexoxo460505@users.noreply.github.com>
2021-05-25 18:15:54 -0700

    Added the 4th form, tried to fix on the 3rd form but failed

---
+++ The Lyrical Lyre/The Lyrical Lyre/Song4Game.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace The_Lyrical_Lyre
+{
+    partial class Song4Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStart = new System.Windows.Forms.Label();
+            this.lbScore = new System.Windows.Forms.Label();
+            this.lbL = new System.Windows.Forms.Label();
+            this.lbK = new System.Windows.Forms.Label();
+            this.lbJ = new System.Windows.Forms.Label();
+            this.lbI = new System.Windows.Forms.Label();
+            this.lbD = new System.Windows.Forms.Label();
+            this.lbS = new System.Windows.Forms.Label();
+            this.lbA = new System.Windows.Forms.Label();
+            this.lbW = new System.Windows.Forms.Label();
+            this.picBorder = new System.Windows.Forms.PictureBox();
+            this.picLBox = new System.Windows.Forms.PictureBox();
+            this.picKBox = new System.Windows.Forms.PictureBox();
+            this.picIBox = new System.Windows.Forms.PictureBox();
+            this.picJBox = new System.Windows.Forms.PictureBox();
+            this.picDBox = new System.Windows.Forms.PictureBox();
+            this.picSBox = new System.Windows.Forms.PictureBox();
+            this.picWBox = new System.Windows.Forms.PictureBox();
+            this.picABox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBorder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picABox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbStart
+            // 
+            this.lbStart.AutoSize = true;
+            this.lbStart.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStart.Location = new System.Drawing.Point(286, 238);
+            this.lbStart.Name = "lbStart";
+            this.lbStart.Size = new System.Drawing.Size(230, 33);
+            this.lbStart.TabIndex = 56;
+            this.lbStart.Text = "Press \"Enter\" to Start";
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.BackColor = System.Drawing.Color.Transparent;
+            this.lbScore.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbScore.Location = new System.Drawing.Point(6, 25);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(41, 19);
+            this.lbScore.TabIndex = 55;
+            this.lbScore.Text = "label1";
+            // 
+            // lbL
+            // 
+            this.lbL.AutoSize = true;
+            this.lbL.BackColor = System.Drawing.Color.Black;
+            this.lbL.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbL.ForeColor = System.Drawing.Color.White;
+            this.lbL.Location = new System.Drawing.Point(779, 405);
+            this.lbL.Name = "lbL";
+            this.lbL.Size = new System.Drawing.Size(20, 21);
+            this.lbL.TabIndex = 54;
+            this.lbL.Text = "L";
+            // 
+            // lbK
+            // 
+            this.lbK.AutoSize = true;
+            this.lbK.BackColor = System.Drawing.Color.Black;
+            this.lbK.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbK.ForeColor = System.Drawing.Color.White;
+            this.lbK.Location = new System.Drawing.Point(666, 405);
+            this.lbK.Name = "lbK";
+            this.lbK.Size = new System.Drawing.Size(21, 21);
+            this.lbK.TabIndex = 53;
+            this.lbK.Text = "K";
+            // 
+            // lbJ
+            // 
+            this.lbJ.AutoSize = true;
+            this.lbJ.BackColor = System.Drawing.Color.Black;
+            this.lbJ.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJ.ForeColor = System.Drawing.Color.White;
+            this.lbJ.Location = new System.Drawing.Point(555, 405);
+            this.lbJ.Name = "lbJ";
+            this.lbJ.Size = new System.Drawing.Size(21, 21);
+            this.lbJ.TabIndex = 52;
+            this.lbJ.Text = "J";
+            // 
+            // lbI
+            // 
+            this.lbI.AutoSize = true;
+            this.lbI.BackColor = System.Drawing.Color.Black;
+            this.lbI.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbI.ForeColor = System.Drawing.Color.White;
+            this.lbI.Location = new System.Drawing.Point(450, 405);
+            this.lbI.Name = "lbI";
+            this.lbI.Size = new System.Drawing.Size(15, 21);
+            this.lbI.TabIndex = 51;
+            this.lbI.Text = "I";
+            // 
+            // lbD
+            // 
+            this.lbD.AutoSize = true;
+            this.lbD.BackColor = System.Drawing.Color.Black;
+            this.lbD.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbD.ForeColor = System.Drawing.Color.White;
+            this.lbD.Location = new System.Drawing.Point(345, 405);
+            this.lbD.Name = "lbD";
+            this.lbD.Size = new System.Drawing.Size(21, 21);
+            this.lbD.TabIndex = 50;
+            this.lbD.Text = "D";
+            // 
+            // lbS
+            // 
+            this.lbS.AutoSize = true;
+            this.lbS.BackColor = System.Drawing.Color.Black;
+            this.lbS.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbS.ForeColor = System.Drawing.Color.White;
+            this.lbS.Location = new System.Drawing.Point(236, 405);
+            this.lbS.Name = "lbS";
+            this.lbS.Size = new System.Drawing.Size(20, 21);
+            this.lbS.TabIndex = 49;
+            this.lbS.Text = "S";
+            // 
+            // lbA
+            // 
+            this.lbA.AutoSize = true;
+            this.lbA.BackColor = System.Drawing.Color.Black;
+            this.lbA.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbA.ForeColor = System.Drawing.Color.White;
+            this.lbA.Location = new System.Drawing.Point(131, 405);
+            this.lbA.Name = "lbA";
+            this.lbA.Size = new System.Drawing.Size(21, 21);
+            this.lbA.TabIndex = 48;
+            this.lbA.Text = "A";
+            // 
+            // lbW
+            // 
+            this.lbW.AutoSize = true;
+            this.lbW.BackColor = System.Drawing.Color.Black;
+            this.lbW.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbW.ForeColor = System.Drawing.Color.White;
+            this.lbW.Location = new System.Drawing.Point(33, 405);
+            this.lbW.Name = "lbW";
+            this.lbW.Size = new System.Drawing.Size(25, 21);
+            this.lbW.TabIndex = 47;
+            this.lbW.Text = "W";
+            // 
+            // picBorder
+            // 
+            this.picBorder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBorder.BackColor = System.Drawing.Color.Black;
+            this.picBorder.Location = new System.Drawing.Point(-4, 405);
+            this.picBorder.Name = "picBorder";
+            this.picBorder.Size = new System.Drawing.Size(844, 10);
+            this.picBorder.TabIndex = 46;
+            this.picBorder.TabStop = false;
+            // 
+            // picLBox
+            // 
+            this.picLBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picLBox.BackColor = System.Drawing.Color.Navy;
+            this.picLBox.Location = new System.Drawing.Point(743, 28);
+            this.picLBox.Name = "picLBox";
+            this.picLBox.Size = new System.Drawing.Size(78, 72);
+            this.picLBox.TabIndex = 45;
+            this.picLBox.TabStop = false;
+            // 
+            // picKBox
+            // 
+            this.picKBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picKBox.BackColor = System.Drawing.Color.Chartreuse;
+            this.picKBox.Location = new System.Drawing.Point(637, 28);
+            this.picKBox.Name = "picKBox";
+            this.picKBox.Size = new System.Drawing.Size(78, 72);
+            this.picKBox.TabIndex = 44;
+            this.picKBox.TabStop = false;
+            // 
+            // picIBox
+            // 
+            this.picIBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picIBox.BackColor = System.Drawing.Color.Brown;
+            this.picIBox.Location = new System.Drawing.Point(422, 28);
+            this.picIBox.Name = "picIBox";
+            this.picIBox.Size = new System.Drawing.Size(78, 72);
+            this.picIBox.TabIndex = 43;
+            this.picIBox.TabStop = false;
+            // 
+            // picJBox
+            // 
+            this.picJBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picJBox.BackColor = System.Drawing.Color.Orange;
+            this.picJBox.Location = new System.Drawing.Point(526, 28);
+            this.picJBox.Name = "picJBox";
+            this.picJBox.Size = new System.Drawing.Size(78, 72);
+            this.picJBox.TabIndex = 42;
+            this.picJBox.TabStop = false;
+            // 
+            // picDBox
+            // 
+            this.picDBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picDBox.BackColor = System.Drawing.Color.MidnightBlue;
+            this.picDBox.Location = new System.Drawing.Point(318, 28);
+            this.picDBox.Name = "picDBox";
+            this.picDBox.Size = new System.Drawing.Size(78, 72);
+            this.picDBox.TabIndex = 41;
+            this.picDBox.TabStop = false;
+            // 
+            // picSBox
+            // 
+            this.picSBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picSBox.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.picSBox.Location = new System.Drawing.Point(214, 28);
+            this.picSBox.Name = "picSBox";
+            this.picSBox.Size = new System.Drawing.Size(78, 72);
+            this.picSBox.TabIndex = 40;
+            this.picSBox.TabStop = false;
+            // 
+            // picWBox
+            // 
+            this.picWBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picWBox.BackColor = System.Drawing.Color.GreenYellow;
+            this.picWBox.Location = new System.Drawing.Point(9, 28);
+            this.picWBox.Name = "picWBox";
+            this.picWBox.Size = new System.Drawing.Size(78, 72);
+            this.picWBox.TabIndex = 39;
+            this.picWBox.TabStop = false;
+            // 
+            // picABox
+            // 
+            this.picABox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picABox.BackColor = System.Drawing.Color.Cyan;
+            this.picABox.Location = new System.Drawing.Point(110, 28);
+            this.picABox.Name = "picABox";
+            this.picABox.Size = new System.Drawing.Size(78, 72);
+            this.picABox.TabIndex = 38;
+            this.picABox.TabStop = false;
+            this.picABox.Visible = false;
+            // 
+            // Song4Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.Controls.Add(this.lbStart);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.lbL);
+            this.Controls.Add(this.lbK);
+            this.Controls.Add(this.lbJ);
+            this.Controls.Add(this.lbI);
+            this.Controls.Add(this.lbD);
+            this.Controls.Add(this.lbS);
+            this.Controls.Add(this.lbA);
+            this.Controls.Add(this.lbW);
+            this.Controls.Add(this.picBorder);
+            this.Controls.Add(this.picLBox);
+            this.Controls.Add(this.picKBox);
+            this.Controls.Add(this.picIBox);
+            this.Controls.Add(this.picJBox);
+            this.Controls.Add(this.picDBox);
+            this.Controls.Add(this.picSBox);
+            this.Controls.Add(this.picWBox);
+            this.Controls.Add(this.picABox);
+            this.Name = "Song4Game";
+            this.Text = "Song4Game";
+            ((System.ComponentModel.ISupportInitialize)(this.picBorder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picABox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbStart;
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.Label lbL;
+        private System.Windows.Forms.Label lbK;
+        private System.Windows.Forms.Label lbJ;
+        private System.Windows.Forms.Label lbI;
+        private System.Windows.Forms.Label lbD;
+        private System.Windows.Forms.Label lbS;
+        private System.Windows.Forms.Label lbA;
+        private System.Windows.Forms.Label lbW;
+        private System.Windows.Forms.PictureBox picBorder;
+        private System.Windows.Forms.PictureBox picLBox;
+        private System.Windows.Forms.PictureBox picKBox;
+        private System.Windows.Forms.PictureBox picIBox;
+        private System.Windows.Forms.PictureBox picJBox;
+        private System.Windows.Forms.PictureBox picDBox;
+        private System.Windows.Forms.PictureBox picSBox;
+        private System.Windows.Forms.PictureBox picWBox;
+        private System.Windows.Forms.PictureBox picABox;
+    }
+}
